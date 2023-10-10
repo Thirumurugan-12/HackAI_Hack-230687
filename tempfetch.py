677df25cf868ec6fa041faf0efe794a9c08f29d0
Thirumurugan-12 <sec22cs123@sairamtap.edu.in>
2023-10-10 20:30:27 +0530

    update

--- tempfetch.py
+++ tempfetch.py
@@ -1,7 +1,7 @@
 import requests
 from uagents import Agent, Context
 import asyncio
-
+import time
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
@@ -9,11 +9,18 @@ bluesense = Agent("Bluesense",endpoint=["http://127.0.0.1:8001/submit"])
 
 @bluesense.on_interval(period=300.0)
 async def Tempreach(ctx: Context):
-    ctx.logger.info(f'Temperate Excedded by {int(temp_celsius) - int(limit_temp)}')
+    ctx.logger.info(f'Temperate Excedded by {int(temp_celsius) - int(limit_temp_up)}')
     st.write("Current Temp",temp_celsius)
-    st.write(" Temperature Exceeded the Limit")
+    if int(temp_celsius) not in range(int(limit_temp_down),int(limit_temp_up)+1):
+        st.write(" Temperature subceeded the Limit") 
+        time.sleep(60)
+        loop.stop
+    else:
+        st.write("Normal Temperture")
+        time.sleep(60)
+        loop.stop()
 
-bluesense.start_background_tasks()
+#bluesense.start_background_tasks()
 
 import streamlit as st
 st.title("Temperature Alert")
@@ -21,23 +28,30 @@ st.sidebar.title("Threshold Settings")
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 api_key = open('api_key.txt', 'r').read()
 city = st.text_input("Enter the city name: ")
-limit_temp = st.sidebar.text_input("Enter the Limit of the Temperature ")
+limit_temp_up = st.sidebar.text_input("Enter the Limit of the Temperature (Upper Limit) ")
+limit_temp_down = st.sidebar.text_input("Enter the Limit of the Temperature (Lower Limit) ")
 
+submit = st.button("Submit")
+
+print("down temp = ",limit_temp_down)
+print("up temp = ",limit_temp_up)
 
 def kelvin_to_celsius(temp):
     return temp - 273.15
 
-if city is not None and len(city) > 0:
-    url = base_url + "appid=" + api_key + "&q=" + city
+if submit:
+    if city is not None and len(city) > 0:
+        url = base_url + "appid=" + api_key + "&q=" + city
 
-    response = requests.get(url).json()
-    temp_kelvin = response['main']['temp']
-    temp_celsius = kelvin_to_celsius(temp_kelvin) 
-    print("Ex")
-    if limit_temp is not None and len(limit_temp) > 0:
-        if int(limit_temp) < int(temp_celsius):
-            print("Exceptional Temperature")
-            bluesense.run()
-    st.write("Temp Normal")
-    st.write("Current TEMP",temp_celsius)
+        response = requests.get(url).json()
+        print(response)
+        temp_kelvin = response['main']['temp']
+        temp_celsius = kelvin_to_celsius(temp_kelvin) 
+        print(int(temp_celsius))
+        if limit_temp_up is not None and len(limit_temp_up) > 0 and limit_temp_down is not None and len(limit_temp_down) > 0:
+            if int(temp_celsius) not in range(int(limit_temp_down),int(limit_temp_up)+1):
+                print("Exceptional Temperature")
+                bluesense.run()
+        st.write("Temp Normal")
+        st.write("Current TEMP",temp_celsius)
     #print(response)
